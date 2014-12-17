ed8cdc33c60126b1256d192720da466b4386fb9e
DevinHenderlong <dhenderlong@etm-inc.com>
2014-12-16 16:37:44 -0800

    PulseSync Board
    Added pulse sync board functionality

--- ETM_CAN_USER_CONFIG.c
+++ ETM_CAN_USER_CONFIG.c
@@ -9,6 +9,9 @@
 #include "A36444.h"
 #endif
 
+#ifdef __A36487
+#include "A36487.h"
+#endif
 
 unsigned int global_reset_faults;
 
@@ -36,6 +39,78 @@ void ETMCanSetValueBoardSpecific(ETMCanMessage* message_ptr) {
     ETMAnalogSetOutput(&global_data_A36444.analog_output_low_energy_vprog,message_ptr->word2);
     ETMCanClearBit(&etm_can_status_register.status_word_0, STATUS_BIT_BOARD_WAITING_INITIAL_CONFIG);
     break;
+#endif
+
+
+#ifdef __A36487   //A36487 PULSE SYNC BOARD
+    unsigned int temp;
+  case ETM_CAN_REGISTER_PULSE_SYNC_SET_1_HIGH_ENERGY_TIMING_REG_0:
+      temp = (message_ptr->word3 & 0xFF00) >> 8;
+      psb_params.grid_delay_high3 = temp;
+      temp = message_ptr->word3 & 0x00FF;
+      psb_params.grid_delay_high2 = temp;
+      temp = (message_ptr->word2 & 0xFF00) >> 8;
+      psb_params.grid_delay_high1 = temp;
+      temp = message_ptr->word2 & 0x00FF;
+      psb_params.grid_delay_high0 = temp;
+      temp = (message_ptr->word1 & 0xFF00) >> 8;
+      psb_params.pfn_delay_high = temp;
+      temp = message_ptr->word1 & 0x00FF;
+      psb_params.rf_delay_high = temp;
+      psb_data.counter_config_received |= 0b0001;
+      break;
+
+  case ETM_CAN_REGISTER_PULSE_SYNC_SET_1_HIGH_ENERGY_TIMING_REG_1:
+      temp = (message_ptr->word3 & 0xFF00) >> 8;
+      psb_params.grid_width_high3 = temp;
+      temp = message_ptr->word3 & 0x00FF;
+      psb_params.grid_width_high2 = temp;
+      temp = (message_ptr->word2 & 0xFF00) >> 8;
+      psb_params.grid_width_high1 = temp;
+      temp = message_ptr->word2 & 0x00FF;
+      psb_params.grid_width_high0 = temp;
+      temp = (message_ptr->word1 & 0xFF00) >> 8;
+      psb_params.afc_delay_high = temp;
+      temp = message_ptr->word1 & 0x00FF;
+      psb_params.spare_delay_high = temp;
+      psb_data.counter_config_received |=0b0010;
+      break;
+
+  case ETM_CAN_REGISTER_PULSE_SYNC_SET_1_LOW_ENERGY_TIMING_REG_0:
+      temp = (message_ptr->word3 & 0xFF00) >> 8;
+      psb_params.grid_delay_low3 = temp;
+      temp = message_ptr->word3 & 0x00FF;
+      psb_params.grid_delay_low2 = temp;
+      temp = (message_ptr->word2 & 0xFF00) >> 8;
+      psb_params.grid_delay_low1 = temp;
+      temp = message_ptr->word2 & 0x00FF;
+      psb_params.grid_delay_low0 = temp;
+      temp = (message_ptr->word1 & 0xFF00) >> 8;
+      psb_params.pfn_delay_low = temp;
+      temp = message_ptr->word1 & 0x00FF;
+      psb_params.rf_delay_low = temp;
+      psb_data.counter_config_received |= 0b0100;
+      break;
+
+  case ETM_CAN_REGISTER_PULSE_SYNC_SET_1_LOW_ENERGY_TIMING_REG_1:
+      temp = (message_ptr->word3 & 0xFF00) >> 8;
+      psb_params.grid_width_low3 = temp;
+      temp = message_ptr->word3 & 0x00FF;
+      psb_params.grid_width_low2 = temp;
+      temp = (message_ptr->word2 & 0xFF00) >> 8;
+      psb_params.grid_width_low1 = temp;
+      temp = message_ptr->word2 & 0x00FF;
+      psb_params.grid_width_low0 = temp;
+      temp = (message_ptr->word1 & 0xFF00) >> 8;
+      psb_params.afc_delay_low = temp;
+      temp = message_ptr->word1 & 0x00FF;
+      psb_params.spare_delay_low = temp;
+      psb_data.counter_config_received |= 0b1000;
+      break;
+
+  case ETM_CAN_REGISTER_PULSE_SYNC_SET_1_CUSTOMER_LED_OUTPUT:
+      psb_data.system_state = message_ptr->word0;
+      break;
 #endif
 
 
@@ -81,7 +156,16 @@ void ETMCanExecuteCMDBoardSpecific(ETMCanMessage* message_ptr) {
 #endif
       
       
-      
+#ifdef __A36487     //A36487 PULSE SYNC BOARD
+      case ETM_CAN_REGISTER_PULSE_SYNC_CMD_ENABLE_PULSES:
+          psb_data.enable_pulses = 1;
+          psb_data.can_comm_ok = 1;         //For 200ms Timeout
+          break;
+      case ETM_CAN_REGISTER_PULSE_SYNC_CMD_DISABLE_PULSES:
+          psb_data.enable_pulses = 0;
+          psb_data.can_comm_ok = 1;         //For 200ms Timeout
+          break;
+#endif
       
       
       
@@ -102,6 +186,14 @@ void ETMCanReturnValueBoardSpecific(ETMCanMessage* message_ptr) {
     /*
       Place all board specific return value commands here
     */
+
+#ifdef __A36487     //A36487 PULSE SYNC BOARD
+      case ETM_CAN_REGISTER_PULSE_SYNC_REQUEST_PERSONALITY_MODULE:
+          etm_can_status_register.status_word_0 = psb_data.personality;         //Where do you want this stored????
+          break;
+#endif
+
+
   default:
     etm_can_can_status.can_status_invalid_index++;
     break;
@@ -112,6 +204,11 @@ void ETMCanReturnValueBoardSpecific(ETMCanMessage* message_ptr) {
 void ETMCanResetFaults(void) {
   // Reset faults associated with this board
   global_reset_faults = 1;
+
+#ifdef __A36487
+    psb_faults.reset_faults = 1;
+#endif
+
 }
 
 
@@ -141,6 +238,13 @@ void ETMCanLogCustomPacketC(void) {
 		);
 #endif
 
+#ifdef __A36487
+    ETMCanLogData(ETM_CAN_DATA_LOG_REGISTER_PULSE_SYNC_FAST_TRIGGER_DATA,
+            psb_data.pulses_on,
+            (psb_data.trigger_input << 8) & psb_data.trigger_filtered,
+            (psb_data.grid_width << 8) & psb_data.grid_delay,
+            0);
+#endif
 
 }
 
@@ -169,7 +273,16 @@ void ETMCanLogCustomPacketD(void) {
 		global_data_A36444.analog_input_lambda_heat_sink_temp.reading_scaled_and_calibrated
 		);
 #endif
-  
+
+
+#ifdef __A36487
+    ETMCanLogData(ETM_CAN_DATA_LOG_REGISTER_PULSE_SYNC_SLOW_TIMING_DATA_0,
+            (psb_params.grid_delay_high3 << 8) & psb_params.grid_delay_high2,
+            (psb_params.grid_delay_high1 << 8) &  psb_params.grid_delay_high0,
+            (psb_params.pfn_delay_high << 8) & psb_params.rf_delay_high,
+            (psb_params.grid_width_high3 << 8) & psb_params.grid_width_high2);
+#endif
+
 }
 
 void ETMCanLogCustomPacketE(void) {
@@ -180,6 +293,14 @@ void ETMCanLogCustomPacketE(void) {
 
   // There is no E packet for HV Lamdba, leave blank
 
+#ifdef __A36487
+    ETMCanLogData(ETM_CAN_DATA_LOG_REGISTER_PULSE_SYNC_SLOW_TIMING_DATA_1,
+            (psb_params.grid_width_high1 << 8) & psb_params.grid_width_high0,
+            (psb_params.afc_delay_high << 8) & psb_params.spare_delay_high,
+            (psb_params.grid_delay_low3 << 8) & psb_params.grid_delay_low2,
+            (psb_params.grid_delay_low1 << 8) & psb_params.grid_delay_low0);
+#endif
+
 }
 
 void ETMCanLogCustomPacketF(void) {
@@ -189,6 +310,14 @@ void ETMCanLogCustomPacketF(void) {
   */
 
   // There is no F packet for HV Lamdba, leave blank
+
+#ifdef __A36487
+    ETMCanLogData(ETM_CAN_DATA_LOG_REGISTER_PULSE_SYNC_SLOW_TIMING_DATA_1,
+            (psb_params.pfn_delay_low << 8) & psb_params.rf_delay_low,
+            (psb_params.grid_width_low3 << 8) & psb_params.grid_width_low2,
+            (psb_params.grid_width_low1 << 8) & psb_params.grid_width_low0,
+            (psb_params.afc_delay_low << 8) & psb_params.spare_delay_low);
+#endif
 
 }
 
